432bb878ceb3178c3ca0f3d40aeec85579c1bf32
Alex Loveless <alex@alexloveless.uk>
2023-10-07 20:20:07 +0100

    Tests for polars

--- tests/data/test_dataframe.py
+++ tests/data/test_dataframe.py
@@ -4,19 +4,31 @@ from maeve import Session
 import pandas as pd
 
 
-def test_load_csv(std_maeve_init_kwargs):
+def test_pandas_load_csv(std_maeve_init_kwargs):
     s = Session(**std_maeve_init_kwargs)
-    df = s.cook("TestLoadCSVNoPipeline")
+    df = s.cook("TestLoadPandasCSVNoPipeline")
     assert type(df) is pd.core.frame.DataFrame
 
 
-def test_load_parquet(std_maeve_init_kwargs):
+def test_pandas_load_parquet(std_maeve_init_kwargs):
     s = Session(**std_maeve_init_kwargs)
     df = s.cook("TestPandasLoadParquet")
     assert type(df) is pd.core.frame.DataFrame
 
 
-def test_inline_pipeline(std_maeve_init_kwargs):
+def test_pandas_load_csv_inline_list_pipeline(std_maeve_init_kwargs):
     s = Session(**std_maeve_init_kwargs)
-    df = s.cook("TestLoadCSVWithInlinePipeline")
+    df = s.cook("TestLoadPandasCSVWithInlineListPipeline")
+    assert "dummy" in list(df.columns)
+
+
+def test_pandas_load_csv_inline_dict_pipeline(std_maeve_init_kwargs):
+    s = Session(**std_maeve_init_kwargs)
+    df = s.cook("TestLoadPandasCSVWithInlineDictPipeline")
+    assert "dummy" in list(df.columns)
+
+
+def test_polars_load_csv_inline_dict_pipeline(std_maeve_init_kwargs):
+    s = Session(**std_maeve_init_kwargs)
+    df = s.cook("TestLoadPorlarsCSVWithInlineDictPipeline")
     assert "dummy" in list(df.columns)
