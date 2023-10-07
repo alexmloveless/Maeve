0f6a6bd073c6b6be8517f0193d5e39d314a3bc79
Alex Loveless <alex@alexloveless.uk>
2023-10-07 20:18:45 +0100

    Added support for pandas/polars series. Not tested.

--- maeve/plugins/data/extensions.py
+++ maeve/plugins/data/extensions.py
@@ -35,3 +35,31 @@ class DataFrame:
             self.backend = "polars"
         else:
             self.backend = None
+
+
+@pd.api.extensions.register_series_accessor(g.core.datapackagestub)
+@pl.api.register_series_namespace(g.core.datapackagestub)
+class Series:
+
+    def __init__(self, df):
+        self._df = df
+        self.df_info()
+        self.pandas = PandasDataFrame()
+        self.polars = PolarsDataFrame()
+
+    def mangle_columns(self):
+        return self.backend_func("mangle_columns")
+
+    def backend_func(self, func, *args, **kwargs):
+        return getattr(getattr(self, self.backend), func)(self._df, *args, *kwargs)
+
+    def df_info(self):
+        self.is_pd = True if type(self._df) is pd.core.series.Series else False
+        self.is_pl = True if type(self._df) is pl.series.series.Series else False
+
+        if type(self._df) is pd.core.frame.DataFrame:
+            self.backend = "pandas"
+        elif type(self._df) is pl.series.series.Series:
+            self.backend = "polars"
+        else:
+            self.backend = None
