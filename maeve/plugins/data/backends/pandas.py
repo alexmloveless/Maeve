f49cd7b2676461dad285f57045277fe67b94ffb0
Alex Loveless <alex@alexloveless.uk>
2023-10-25 17:11:09 +0100

    new rel

--- maeve/plugins/data/backends/pandas.py
+++ maeve/plugins/data/backends/pandas.py
@@ -24,7 +24,10 @@ class PandasDataFrame:
         # do stuff
         return df
 
-    def unspace_colnames(self, df: pd.DataFrame, delim: str = "_") -> pd.DataFrame:
+    def unspace_colnames(self,
+                         df: pd.core.dataframe.DataFrame,
+                         delim: str = "_"
+                         ) -> pd.core.dataframe.DataFrame:
         """Datafunc - remove spaces from column names. Default delim is underscore
 
         Parameters
