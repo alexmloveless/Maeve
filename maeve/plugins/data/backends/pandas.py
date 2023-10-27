f535260d5ef2cc6aaa0606ab7414a04928e2d15d
petehodge <pete_hodge@hotmail.com>
2023-10-27 10:07:00 +0100

    Added function replace_column_values and tests to pandas. Outstanding bug in passing kwargs

--- maeve/plugins/data/backends/pandas.py
+++ maeve/plugins/data/backends/pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pydantic import BaseModel, field_validator, conlist
-from typing import Optional, Union
+from typing import Optional, Union, Any
 import re
 
 
@@ -38,6 +38,80 @@ class PandasDataFrame:
         """
 
         df.columns = [re.sub(" ", delim, c) for c in df.columns]
+        return df
+
+    def replace_column_values(self,
+                              df: pd.DataFrame,
+                              change_cols: Union[str, list],
+                              qry: Optional[str] = None,
+                              index: Optional[Any] = None,
+                              replace_val: Optional[Any] = None,
+                              replace_cols: Optional[Union[str, list]] = None,
+                              replace_index: Optional[Any] = None,
+                              multiplier: Optional[float] = None) -> pd.DataFrame:
+        """Replace values with a scalar or values from another column.
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+        change_cols: str or list of strings
+            Column name to act on
+        qry: str, optional
+            Query string to filter rows to be changed. If query is used, then it is automatically used as the replacement
+            index as well.
+        index: Any, optional
+            Index value or slice to filter rows to be changed.
+            This or qry must be entered. If both, qry will be used.
+        replace_val: Any, optional
+            Scalar value to replace current values.
+            Overrides remaining parameters even if they are supplied.
+        replace_cols: str or list of strings, optional
+            Column or list of columns whose values will be used for replacement values
+        replace_index: Any, optional
+            Index of the row or rows to be used for replacement values
+        multiplier: float, optional
+            Used to multiply the new column values by
+
+        Returns
+        -------
+        pd.DataFrame
+
+        Examples
+        --------
+        Update a single value with a scalar:
+            replace_column_values(df, 'streams', index=574, replace_val=0)
+        Update a single value with value from another column, same row:
+            replace_column_values(df, 'streams', index=574, replace_cols='released_year')
+        Update a number of rows with another column using query:
+            replace_column_values(df, 'streams', qry="released_year == 2023", replace_cols='bpm')
+        Update a single value with value from another row, same column with a multiplier:
+            replace_column_values(df, 'streams', index=574, replace_index=575, multiplier=2)
+        """
+        print("Entered replace_column_values.")
+        print(locals())
+        df = df.copy()
+
+        # Query will override index even if supplied
+        if qry:
+            index = replace_index = df.query(qry).index
+        elif index:
+            replace_index = index if not replace_index else replace_index
+        else:
+            self.log.warning("Need query or index parameters.")
+            return df
+
+        replace_cols = change_cols if not replace_cols else replace_cols
+
+        # replace_val overrides replace_index, replace_cols and multiplier even if supplied
+        print(f'repl val {replace_val} cols {change_cols} qry {qry}')
+        if replace_val is not None:
+            df.loc[index, change_cols] = replace_val
+        else:
+            if multiplier is not None:
+                df.loc[index, change_cols] = df.loc[replace_index, replace_cols] * multiplier
+            else:
+                df.loc[index, change_cols] = df.loc[replace_index, replace_cols]
+
         return df
 
     def loc_slice(self, df, index=None, columns=None):
--- tests/core/test_functions.py
+++ tests/core/test_functions.py
@@ -12,3 +12,24 @@ def test_unspace_colnames(std_maeve_init_kwargs):
     df['new col'] = 'Y'
     df = df.mv.unspace_colnames()
     assert 'new_col' in df.columns
+
+def test_replace_column_values(std_maeve_init_kwargs):
+    # Add a column with spaces, then remove spaces
+    s = Session(**std_maeve_init_kwargs)
+    df = s.cook("TestLoadPandasCSVNoPipeline")
+    # Update a single value with a scalar:
+    # df1 = df.mv.replace_column_values('streams', index=574, replace_val=0)
+    # assert df1.loc[574, 'streams'] == 0
+
+    # Update a single value with value from another column, same row:
+    df1 = df.mv.replace_column_values('streams', index=574, replace_cols='released_year')
+    assert df1.loc[574, 'streams'] == 1970
+
+    # Update a row with another column using query:
+    df1 = df.mv.replace_column_values('streams', qry="track_name == 'Love Grows (Where My Rosemary Goes)'",
+                                      replace_cols='bpm')
+    assert df1.loc[574, 'streams'] == 110
+
+    # Update a single value with value from another row, same column with a multiplier:
+    df1 = df.mv.replace_column_values('streams', index=574, replace_index=575, multiplier=2)
+    assert df1.loc[574, 'streams'] == '374706940374706940'
